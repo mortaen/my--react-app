122aea2390e8b623cc526656814e38e1bbc006a1
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-14 14:31:36 +0200

    add Footer.js and Footer.css

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <button className="Footer__button red">Gryffindor</button>
+      <button className="Footer__button yellow">Hufflepuff</button>
+      <button className="Footer__button blue">Ravenclaw</button>
+      <button className="Footer__button green">Slytherin</button>
+    </footer>
+  )
+}
+
+export default Footer
