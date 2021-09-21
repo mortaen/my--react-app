4a807c9a6441f5128c3b740fad091e09f800c71e
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-21 17:21:22 +0200

    change import and data name in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./components/app/App"
+import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import data from "./data.json"
+import harryPotterData from "./data.json"
 import GlobalStyles from "./GlobalStyles"
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App data={data} />
+    <App harryPotterData={harryPotterData} />
   </React.StrictMode>,
   document.getElementById("root")
 )
