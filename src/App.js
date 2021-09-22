6f5589278424ea48afc4a0cbc4411d32cf02b2b9
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-22 11:31:11 +0200

    add start page and links to routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Route, Switch } from "react-router"
+import { Link, Route, Switch } from "react-router-dom"
 import { BrowserRouter as Router } from "react-router-dom"
+import styled from "styled-components/macro"
 import FormApp from "./components/FormApp/FormApp"
 import HarryPotterApp from "./components/harry-potter-app/HarryPotterApp"
 
@@ -8,16 +9,31 @@ function App({ harryPotterData }) {
     <>
       <Router>
         <Switch>
+          <Route exact path="/">
+            Startseite
+            <StyledLink to="/form">Form</StyledLink>
+            <StyledLink to="/harrypotter">Harry Potter App</StyledLink>
+          </Route>
           <Route exact path="/form">
             <FormApp />
           </Route>
           <Route exact path="/harrypotter">
             <HarryPotterApp data={harryPotterData} />
           </Route>
+          <Route>404 NOT FOUND</Route>
         </Switch>
       </Router>
     </>
   )
 }
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  background-color: lightsalmon;
+  padding: 5px;
+  text-decoration: none;
+  border-radius: 7px;
+  color: darkcyan;
+`
 
 export default App
